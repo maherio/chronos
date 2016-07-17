a2118c6e51bf8d76f2b648730f89b3912f325599
Tony Maher <amaher@alumni.nd.edu>
2016-07-17 15:39:01 -0500

    Adding fresh Equip installation.

---
+++ web/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Maherio\Chronos\Domain;
+
+Equip\Application::build()
+->setConfiguration([
+    Equip\Configuration\AurynConfiguration::class,
+    Equip\Configuration\DiactorosConfiguration::class,
+    Equip\Configuration\PayloadConfiguration::class,
+    Equip\Configuration\RelayConfiguration::class,
+    Equip\Configuration\WhoopsConfiguration::class,
+])
+->setMiddleware([
+    Relay\Middleware\ResponseSender::class,
+    Equip\Handler\ExceptionHandler::class,
+    Equip\Handler\DispatchHandler::class,
+    Equip\Handler\JsonContentHandler::class,
+    Equip\Handler\FormContentHandler::class,
+    Equip\Handler\ActionHandler::class,
+])
+->setRouting(function (Equip\Directory $directory) {
+    return $directory
+    ->get('/', Domain\Welcome::class)
+    ;
+})
+->run();
